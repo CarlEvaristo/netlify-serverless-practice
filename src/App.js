0bc70cb39ed5e83d813f4b658a6718f9ce55ffc8
CarlEverest78 <thepythoneer@gmail.com>
2022-10-04 16:40:33 +0200

    add quote api request, run request on  button click and save sto state, paragraph  element displays response text

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import axios from "axios"
 
 function App() {
-  // const [quote, setQuote] = React.useState("")
-  // const URL = "https://ron-swanson-quotes.herokuapp.com/V2/QUOTES"
+  const [quote, setQuote] = React.useState("")
+  const URL = "https://ron-swanson-quotes.herokuapp.com/V2/QUOTES"
 
-  // function getQoute() {
-  //   axios.get(URL)
-  //     .then(res => console.log(res))
-  // }
+  function getQoute() {
+    axios.get(URL)
+      .then(res => setQuote(res))
+  }
 
   return (
     <div className="App">
       <h1>Ron Swanson Quote Generator</h1>
-      {/* <button onClick={getQoute}>Get Quote</button>
-      <p>Quote: {quote.data[0]}</p> */}
+      <button onClick={getQoute}>Get Quote</button>
+      <p>Quote: {quote.data[0]}</p>
     </div>
   );
 }
