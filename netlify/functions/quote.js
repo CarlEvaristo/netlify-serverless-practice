55167d4547cd890252bee2a2c3a80b3541f7f817
CarlEverest78 <thepythoneer@gmail.com>
2022-10-05 09:10:43 +0200

    testing env vars

---
+++ netlify/functions/quote.js
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+exports.handler = function(event, context, callback) {
+    axios.get("https://ron-swanson-quotes.herokuapp.com/V2/QUOTES")
+        .then(res =>  callback(null,{
+            statusCode: 200,
+            body: JSON.stringify(res.data)
+            })
+        )
+}
--- src/App.js
+++ src/App.js
@@ -6,9 +6,16 @@ function App() {
   const [greeting, setGreeting] = React.useState("")
   const [time, setTime] = React.useState("")
 
-  function getQoute() {
-    axios.get("https://ron-swanson-quotes.herokuapp.com/V2/QUOTES")
-      .then(res => setQuote(res))
+  // function getQoute() {
+  //   axios.get("https://ron-swanson-quotes.herokuapp.com/V2/QUOTES")
+  //     .then(res => setQuote(res))
+  // }
+  
+  function getQuoteThroughServer() {
+    fetch("/api/quote")
+    .then(res => res.json())
+    .then(res => setQuote(res))
+    .catch(err => console.log("ERROR: ", err))
   }
 
   function getGreeting(event){
@@ -30,8 +37,8 @@ function App() {
     <div className="App">
       <h1>Ron Swanson Quote Generator</h1>
       <h2>{time}</h2>
-      <button onClick={getQoute}>Get Quote</button>
-      <p>Quote: {quote.data}</p>
+      <button onClick={getQuoteThroughServer}>Get Quote</button>
+      <p>Quote: {quote}</p>
       <button onClick={getGreeting}>Get Greeting</button>
       <p>Say: {greeting}</p>
     </div>
