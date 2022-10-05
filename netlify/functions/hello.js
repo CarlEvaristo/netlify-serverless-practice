aae83d1a24e217746f5543297b578757f08e232f
CarlEverest78 <thepythoneer@gmail.com>
2022-10-05 06:55:16 +0200

    add 1st serverless function to test

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,10 +1,6 @@
-
 exports.handler = function(event, context, callback){
-    // event === React request(body) object (contains stuff like: headers,http method(get post etc),)
-    // context === authentication and identity information (id's, keys, etc)
-    // callback === React response object
-    callback(null, {    //=> 1st param = error object (set to null -no error)  => 2nd param = response object
+    callback(null, {    
         statusCode: 200,
-        body: JSON.stringify("Hello World")         //=> response body === json object !!
+        body: JSON.stringify("Hello World")       
     })
 }
