02bbd794ec819179e0e54babba1a04686e199cc6
CarlEverest78 <thepythoneer@gmail.com>
2022-10-05 08:07:47 +0200

    add redirects

--- netlify/functions/time.js
+++ netlify/functions/time.js
@@ -1,5 +1,7 @@
 exports.handler = function(event, context, callback) {
     const date = new Date()
+    const key = process.env.REACT_APP_API_KEY
+    console.log(key)
     callback(null, {
         statusCode:200,
         body: JSON.stringify(date)
