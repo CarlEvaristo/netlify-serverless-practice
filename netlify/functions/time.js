b38dec03a3900a076cc1a0f6011a18ded6e77a51
CarlEverest78 <thepythoneer@gmail.com>
2022-10-05 07:49:30 +0200

    add redirects

--- netlify/functions/time.js
+++ netlify/functions/time.js
@@ -2,8 +2,8 @@ exports.handler = function(event, context, callback) {
     const date = new Date()
     const year = date.getFullYear()
     callback(null, {
-        statCode:200,
-        body: JSON.stringify("Hello World")
+        statusCode:200,
+        body: JSON.stringify(year)
     })
 }
 
