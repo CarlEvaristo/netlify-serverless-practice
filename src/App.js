d9ef60dc87c27bd2e39c79fac0484d6b409a7534
CarlEverest78 <thepythoneer@gmail.com>
2022-10-05 19:07:47 +0200

    first version with mongo - test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import axios from "axios"
 
 function App() {
   const [quote, setQuote] = React.useState("")
   const [greeting, setGreeting] = React.useState("")
-  const [db, setDb] = React.useState("")
-
-  // function getQoute() {
-  //   axios.get("https://ron-swanson-quotes.herokuapp.com/V2/QUOTES")
-  //     .then(res => setQuote(res))
-  // }
   
   function getQuoteThroughServer() {
     fetch("/api/quote")
@@ -25,14 +18,6 @@ function App() {
       .catch(err => console.log("ERROR: ", err))
   }
 
-  function getDB(){
-    fetch("/api/mongo")
-      .then(res => res.json())
-      .then(res => setDb(res))
-      .catch(err => console.log("ERROR: ", err))
-  }
-
-
   return (
     <div className="App">
       <h1>Ron Swanson Quote Generator</h1>
@@ -43,8 +28,6 @@ function App() {
       <button onClick={getGreeting}>Get Greeting</button>
       <p>Say: {greeting}</p>
 
-      <button onClick={getDB}>Get DB</button>
-      <p>DB: {db}</p>
     </div>
   );
 }
