57000d19d669f39d1117c271413464dd7ee7bfa1
CarlEverest78 <thepythoneer@gmail.com>
2022-10-05 08:10:13 +0200

    testing env vars

--- netlify/functions/time.js
+++ netlify/functions/time.js
@@ -1,10 +1,9 @@
 exports.handler = function(event, context, callback) {
     const date = new Date()
     const key = process.env.REACT_APP_API_KEY
-    console.log(key)
     callback(null, {
         statusCode:200,
-        body: JSON.stringify(date)
+        body: JSON.stringify(`${date}, ${key}`)
     })
 }
 
